79204838df19ff9b825504b221f601b1885a9cde
Vishalkod <144665345+Vishalkod@users.noreply.github.com>
2023-10-02 23:21:17 +0530

    Update task_4.py

--- Phase2_project_Task4/task_4.py
+++ Phase2_project_Task4/task_4.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-# Step 1: Load and preprocess the data
+# Load and preprocess the data
 train_data = pd.read_csv('train_dataset.csv')
 test_data = pd.read_csv('test_dataset.csv')
 
@@ -12,11 +12,11 @@ y_train = train_data['y']
 X_test = test_data[['x']]
 y_test = test_data['y']
 
-# Step 2: Train the linear regression model
+# Train the linear regression model
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-# Step 3: Make predictions on the test dataset
+# Make predictions on the test dataset
 y_pred = model.predict(X_test)
 
 # Calculate the Mean Squared Error (MSE) to evaluate the model
